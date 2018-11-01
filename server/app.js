c8eed7ab4db65c596d501e4d0159af38f98987ed
Sagar Ghimire <sagar@growcept.com>
2018-11-01 19:00:31 +1100

    Add BookList Component

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors =  require('cors');
 
 const app = express();
+
+//allow cross origin request
+app.use(cors());
 
 //connect to mongo
 mongoose.connect('mongodb://gmrsagar:s11030479@ds139193.mlab.com:39193/gql-mongo');
