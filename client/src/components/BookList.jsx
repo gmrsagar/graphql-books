0d7d23a20eaa90512f0c45da77793cbb49e84ff2
Sagar Ghimire <sagar@growcept.com>
2018-11-04 21:12:21 +1100

    Add Single Book Query

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
   displayBooks() {
     let data = this.props.data
     if(data.loading) {
@@ -11,7 +17,7 @@ class BookList extends Component {
     } else {
       return data.books.map(book => {
         return (
-          <li key = {book.id}>{ book.name }</li>
+          <li onClick={(e) => { this.setState({ selected: book.id })}} key = {book.id}>{ book.name }</li>
         )
       })
     }
@@ -23,6 +29,7 @@ class BookList extends Component {
         <ul id="book-list">
          {this.displayBooks()}
         </ul>
+        <BookDetails bookId={this.state.selected}/>
       </div>
     )
   }
